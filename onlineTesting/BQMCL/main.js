3ccd47c53cc5fe94fe7e21cba719267f36ce1a7a
MCGodly <55669250+MCGodly@users.noreply.github.com>
2025-07-31 00:02:27 +0000

    mod

---
+++ onlineTesting/BQMCL/main.js
@@ -0,0 +1,46 @@
+var nightShiftChecklist = {
+	stockCoffeeIsland: {id: "stockCoffeeIsland", simpleName: "Stock The Coffee Island", completed: false},
+	sanitizeIslandCounter: {id: "sanitizeIslandCounter", simpleName: "Sanitize The Island Counter", completed: false}
+}
+
+window.onload = () => {
+	document.getElementById("settingsButton").onclick = ()=>{
+		document.getElementById("settingsSidebar").style.left = "0px";
+	}
+	document.getElementById("backArrow").onclick = ()=>{
+		document.getElementById("settingsSidebar").style.left = "-250px";
+	}
+	
+	var nightShiftArray = Object.keys(nightShiftChecklist);
+	for(var i=0;i<nightShiftArray.length;i++) {
+		var currentItem = nightShiftChecklist[nightShiftArray[i]];
+		var a = document.createElement("li");
+		a.innerHTML = `
+		<h2 class="listTitle">`+currentItem.simpleName+`</h2>
+		<div class="doneSpanContainer" customVar="`+currentItem.id+`">
+			<div class="doneSpan" style="animation-delay: -`+Math.floor(Math.random()*20)/10+`s">Not Done</div>
+		</div>
+		`;
+		document.getElementById("list").appendChild(a);
+	}
+	
+	var doneLabels = document.getElementsByClassName("doneSpanContainer");
+	for(var i=0;i<doneLabels.length;i++) {
+		var doneLabel = doneLabels[i];
+		
+		doneLabel.setAttribute("onclick", `
+			var item = nightShiftChecklist.`+doneLabel.getAttribute("customVar")+`;
+			item.completed = !item.completed;
+			if(item.completed) {
+				this.children[0].style.color = "lightgreen";
+				this.children[0].style.animationName = "none";
+				this.children[0].innerHTML = "Done";
+			} else {
+				this.children[0].style.color = "#FF3333";
+				this.children[0].style.animationName = "notDoneSpan";
+				this.children[0].innerHTML = "Not Done";
+			}
+			`);
+	}
+	
+};
