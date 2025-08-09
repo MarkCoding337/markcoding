f192716901ba0278fb0a72cc6a8f590babffe526
MCGodly <55669250+MCGodly@users.noreply.github.com>
2025-08-09 19:31:02 +0000

    mod

--- onlineTesting/BQMCL/main.js
+++ onlineTesting/BQMCL/main.js
@@ -4,7 +4,7 @@ var nightShiftChecklist = {
 		simpleName: "Stock The Coffee Island",
 		detail: 
 `Stock:
-	Brunn Cups & Lids,
+	Bunn Cups & Lids,
 	Soda Cups & Lids,
 	Torke Cups & Lids,
 	Coffee Creamers,
@@ -58,7 +58,9 @@ var nightShiftChecklist = {
 Clean:
 	Catch-Tray For Silver Pot,
 	Catch-Tray For Capucino Machine,
-	Catch-Tray For Brunn Machine,
+	Catch-Tray For Bunn Machine,
+	Backing Of Capucino Machine,
+	Backing Of Soda Machine,
 `,
 		completed: false
 	},
@@ -66,9 +68,41 @@ Clean:
 		id: "sodaMachine",
 		simpleName: "Soda Machine Care",
 		detail:
-`Take heads off of soda fountain, then put them into hot water overnight.
+`Dump hot water into the tray of the soda fountain, turn off the machine (Turn key to the vertical position), take heads off and put them into hot water overnight.
 `,
-	}
+	},
+	hotFoodRemoval: {
+		id: "hotFoodRemoval",
+		simpleName: "Remove Hot Food",
+		detail:
+`Take hot food from both cases, place any foil wrapped food directly into its respective bin just in the freezer to your left on the lowest level (Labeled by weekday). Any other food should be placed into a plastic bag and closed with a twist-tie (Or equivalent) and place into freezer.`,
+	},
+	stockShelves: {
+		id: "stockShelves",
+		simpleName: "Stock Shelves & Face Store",
+		detail:
+`Stock:
+	Liquor shelves,
+	Butter & Eggs on cold island,
+Face Store:
+	Make sure products are neat on the shelves and in their appropriate labeled spots (If there is no label on the shelf just make sure it looks neat and that identical products are together).
+	`
+	},
+	donutSALEEE: {
+		id: "donutSALEEE",
+		simpleName: "Put Up Donut Sign",
+		detail:
+`After 5 P.M. make sure the donut sale sign located above the bakery thingie is flipped so that customers can see it.`
+	},
+	coldDeli: {
+		id: "coldDeli",
+		simpleName: "Cold Deli",
+		detail:
+`Remove cold deli condiments from tray located to the left of the big hot food case and place them in the sandwich case to where they aren't in the way of customers seeing sandwiches.
+
+Make sure that the cold deli tray is turned off when you are done* (Switch Angled Downward)`
+	},
+
 	
 }
 
