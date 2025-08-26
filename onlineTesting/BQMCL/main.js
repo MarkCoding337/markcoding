d9dfd06eff73a90d7daa1eb24097b63f296ed62b
MCGodly <55669250+MCGodly@users.noreply.github.com>
2025-08-26 19:34:08 +0000

    mod

--- onlineTesting/BQMCL/main.js
+++ onlineTesting/BQMCL/main.js
@@ -68,14 +68,16 @@ Clean:
 		id: "sodaMachine",
 		simpleName: "Soda Machine Care",
 		detail:
-`Dump hot water into the tray of the soda fountain, turn off the machine (Turn key to the vertical position), take heads off and put them into hot water overnight.
+`	Dump hot water into the tray of the soda fountain, turn off the machine (Turn key to the vertical position), take heads off and put them into hot water overnight.
 `,
+		completed: false
 	},
 	hotFoodRemoval: {
 		id: "hotFoodRemoval",
 		simpleName: "Remove Hot Food",
 		detail:
-`Take hot food from both cases, place any foil wrapped food directly into its respective bin just in the freezer to your left on the lowest level (Labeled by weekday). Any other food should be placed into a plastic bag and closed with a twist-tie (Or equivalent) and place into freezer.`,
+`	Take hot food from both cases, place any foil wrapped food directly into its respective bin just in the freezer to your left on the lowest level (Labeled by weekday). Any other food should be placed into a plastic bag and closed with a twist-tie (Or equivalent) and place into freezer.`,
+		completed: false
 	},
 	stockShelves: {
 		id: "stockShelves",
@@ -86,23 +88,126 @@ Clean:
 	Butter & Eggs on cold island,
 Face Store:
 	Make sure products are neat on the shelves and in their appropriate labeled spots (If there is no label on the shelf just make sure it looks neat and that identical products are together).
-	`
+	`,
+		completed: false
 	},
 	donutSALEEE: {
 		id: "donutSALEEE",
 		simpleName: "Put Up Donut Sign",
 		detail:
-`After 5 P.M. make sure the donut sale sign located above the bakery thingie is flipped so that customers can see it.`
+`	After 5 P.M. make sure the donut sale sign located above the bakery thingie is flipped so that customers can see it.`,
+		completed: false
 	},
 	coldDeli: {
 		id: "coldDeli",
 		simpleName: "Cold Deli",
 		detail:
-`Remove cold deli condiments from tray located to the left of the big hot food case and place them in the sandwich case to where they aren't in the way of customers seeing sandwiches.
+`	Remove cold deli condiments from tray located to the left of the big hot food case and place them in the sandwich case to where they aren't in the way of customers seeing sandwiches.
 
-Make sure that the cold deli tray is turned off when you are done* (Switch Angled Downward)`
+	Make sure that the cold deli tray is turned off when you are done* (Switch Angled Downward)`,
+		completed: false
 	},
+	stockStok: {
+		id: "stockStok",
+		simpleName: "Stock the Sto&#772;k",
+		detail:
+`	Open Sto&#772;k machine face (latch on the right side) and check if bags of iced coffee are empty. If either bags are empty take them out of the machine and remove the bag from the white container. In the cooler on your immediate right there should be boxes with the Sto&#772;k coffee bags inside (they may be underneath some other stuff). If there are no extra bags in the cooler just make sure to label that flavor as "Unavailable" and put the white container back into the machine.
+`,
+		completed: false
+	},
+	pizzaStock: {
+		id: "pizzaStock",
+		simpleName: "Keep Pizza Stocked",
+		detail:
+`	Before about 7:00P.M. (vary based on how busy it is by then), make sure there is at least one pizza in the hot case, if there isn't, then make one from underneath the deli prep table (middle cabinet door). If there are none in there, then check the cooler to the right of the dairy stock. Just make sure that you grab a labeled one ("Pep", "Saus", etc.) and that it is a 13in and not one of the larger ones.
+	Once you bring it back to the deli prep table: 
+		Wash your hands,
+		Remove top of pizza container,
+		Bring pizza (in metal dish) to the bottom oven,
+		Place pizza on the (slowly) moving rack,
+		Wait for it to come out the other side (please continue cashiering in the meantime, check for it every 2-3mins),
+		USING OVEN MITS... Grab the pizza dish and bring it to the cutting board (should be on the table underneath the deli phone, if not it may be in the dishwasher or somewhere else in the deli),
+		Take the pizza from the dish and place it on the cutting board (Please don't flip pizza),
+		Using the pizza cutter, follow the grooves in the cutting board and cut the pizza into 6 similar sized slices,
+		Grab 6 cardboard pizza slice trays from underneath the oven and label them on the upper flap with whatever flavor of pizza you made,
+		Make sure you have one of the black trays for the pizza hot case ready and (using the metal pizza spatula) move one slice at a time from the cutting board over to a carboard tray and place the carboard tray onto the black tray,
+		Once all 6 slices are in their carboard and on a black tray, take it to the hot case and set the whole tray on one of the empty shelves.
 
+`,
+		completed: false
+	},
+	takeTemps: {
+		id: "takeTemps",
+		simpleName: "Take Temperatures",
+		detail:
+`	Grab the checklist (To the left of the computer) and find the first and second temps pages (one is "cold", and the other is "frozen" [except it also has bait fridge]), find the current date as labeled on the left side of each page and fill in the temperature of each "cooler" or "freezer". 
+	
+	[] Where is each thing?
+		- Sandwich Display: Self explaining... The temperature gauge though is really small and located on the right when you open the right backdoor from inside the deli (Radial Gauge),
+		- Deli-Prep Top&Bottom: Both temps are to be taken from the digital temperature readout on the absolute bottom left of the deli prep table (Just inside the deli to your left),
+		- Produce Top, Mid, & Bottom: The walkaround cooler with all the cheese has 3 different layers, each layer has a temperature gauge on the long side facing the main cooler. Two of them are bar gauges and one is a radial gauge.,
+		- Cooler: The cooler has a gauge just to the right of its door (Radial Gauge),
+		
+		- Novelty Freezer: The freezer with the slidey top that is embedded into the cashier counter on the front left side. The gauge is located inside on the back of the freezer towards the right (Radial Gauge),
+		- Hand Scoop: The freezer accessible from behind the cashier counter. The gauge is visibile on the back wall of the freezer. (Radial Gauge),
+		- Freezer: The freezer in the deli with the big door. The temperature gauge is located to the right of the door. (Radial Gauge),
+		
+		- Bait: The Bait Fridge&#8482; is located near the main entrance to the store very close to the indoor ice freezer. The gauge is somewhere in the fridge and looks very much alike to the bar styles temperature gauges found on the Top & Mid of the walkaround cooler.
+`,
+		completed: false
+	},
+	garbageCardboard: {
+		id: "garbageCardboard",
+		simpleName: "Take Out Trash and Recycle",
+		detail: 
+`	At least once in during your shift all the outside garbages should be checked and replaced if they are above half of their capacity.
+
+	All garbages inside should also be changed if they become too full, though they need not be changed immediately when they are above half as long as they don't get completely full. This includes the small basket by the ATM and the garbage in the back hallway.
+	
+	You also must take out the recycling (the other bin in the back [whichever one that is filled with plastic and other recyclables]) and put it in the dumpster next to the garbage dumpster.
+	
+	There's also the carboard cart in back that should be emptied and the cardboard put into the same dumpster as the recyclables.
+`,
+		completed: false
+	},
+	
+	closeDoors: {
+		id: "closeDoors",
+		simpleName: "Make sure doors are closed",
+		detail:
+`	Once it is past 10:00P.M. (9P.M. on Sundays) and there are no customers inside the building (check the bathrooms), you can lock the 4 front doors by turning the little silver dial towards the center piece between the two doors for both sets of doors.`,
+		completed: false
+	},
+	vacuum: {
+		id: "vacuum",
+		simpleName: "Vacuum Rugs",
+		detail:
+`	Vacuum the rugs by the cashier station (One in front of each till and the third between the cashier area and the deli) as well as the two rugs by the main doors.
+	*The rugs by the cashier station should also be rolled up after they have been vacuumed.`,
+		completed: false
+	},
+	floors: {
+		id: "floors",
+		simpleName: "Clean the Floors",
+		detail:
+`	- Sweep the floors (Consider using the dry mop to sweep as it is larger, though to get under the lip of some shelves you may need to use the traditional broom),
+	- Once the floors are swept you can mop the floor, make sure to scrub out heavy dirt by going over it repetitively until it's gone (if it isn't gone after a bit of scrubbing you can just move on)`,
+		completed: false
+	},
+	cashierCare: {
+		id: "cashierCare",
+		simpleName: "Keep Cashier Area Clean",
+		detail:
+`	Make sure that the cashier area is maintained and looks neat, this includes not leaving garbage laying around and sweeping the floor.`,
+		completed: false
+	},
+	bathroomCare: {
+		id: "bathroomCare",
+		simpleName: "Keep Bathroom Maintained and Clean",
+		detail:
+``,
+		completed: false
+	}
 	
 }
 
