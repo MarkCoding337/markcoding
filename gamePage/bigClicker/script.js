4ca2616ee6e1c56102645d4e1f3362fdb03a16f5
Mark <55669250+MarkCoding337@users.noreply.github.com>
2025-10-22 04:14:08 +0000

    mod

--- gamePage/bigClicker/script.js
+++ gamePage/bigClicker/script.js
@@ -131,9 +131,9 @@ var game = {
         a.className = "";
       };
       if(b[i].scaleT == 0) {
-        b[i].price = (b[i].initialPrice+((b[i].priceScale*(b[i].num+game.purchaseInterval))));
+        b[i].price = (b[i].initialPrice+((b[i].priceScale*(b[i].num+game.purchaseInterval-1n))));
       } else if(b[i].scaleT == 1) {
-        b[i].price = b[i].initialPrice+((b[i].priceScale)**(b[i].num+game.purchaseInterval));
+        b[i].price = b[i].initialPrice+((b[i].priceScale)**(b[i].num+game.purchaseInterval-1n));
       }
       console.log(b[i].price);
       a.innerHTML = b[i].name + " | Price: $"+game.dispNum(b[i].price*game.purchaseInterval)+" For "+game.dispNum(game.purchaseInterval)+" Unit | Have: "+game.dispNum(b[i].num);
@@ -156,7 +156,7 @@ var game = {
         if(b[i].scaleT == 0) {
           b[i].price = (b[i].initialPrice+((b[i].priceScale*b[i].num)));
         } else if(b[i].scaleT == 1) {
-          b[i].price = b[i].initialPrice+((b[i].priceScale)**(b[i].num+1n));
+          b[i].price = b[i].initialPrice+((b[i].priceScale)**(b[i].num));
         }
         a.innerHTML = b[i].name + " | Price: $"+game.dispNum(b[i].price)+" | Have: "+game.dispNum(b[i].num) + " | Max: "+game.dispNum(b[i].max);
         a.idNUM = i;
