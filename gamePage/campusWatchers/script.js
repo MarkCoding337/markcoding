6b42166738f87544d6aef6469258ca42b235e395
Mark <55669250+MarkCoding337@users.noreply.github.com>
2025-11-06 06:33:26 +0000

    mod

---
+++ gamePage/campusWatchers/script.js
@@ -0,0 +1,35 @@
+var g = {
+    camera: {},
+};
+window.onload = function() {
+    g.camera.ele = document.getElementById('cameraViewer');
+    g.camera.width = g.camera.ele.clientWidth; // 300% of container
+    g.camera.height = g.camera.ele.clientHeight; // 100% of container
+    g.camera.position = 1;
+    g.camera.ele.style.left = `-${g.camera.width/3}px`;
+    g.camera.ele.onclick = function(e) {
+        if(e.clientX < 100){
+            if(g.camera.position == 1){
+                this.style.left = `0px`;
+                console.log("left");
+                g.camera.position = 0;
+            }
+            else if(g.camera.position == 2){
+                this.style.left = `-${g.camera.width/3}px`;
+                console.log("center");
+                g.camera.position = 1;
+            }
+        } else if(e.clientX > window.innerWidth - 100) {
+            if(g.camera.position == 0){
+                this.style.left = `-${g.camera.width/3}px`;
+                console.log("center");
+                g.camera.position = 1;
+            }
+            else if(g.camera.position == 1){
+                this.style.left = `-${g.camera.width/3*2}px`;
+                console.log("right");
+                g.camera.position = 2;
+            }
+        }
+    }
+}
