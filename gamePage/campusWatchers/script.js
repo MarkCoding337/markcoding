93dedd1fb9ba6bf5adfbe78cc3d110bf99c8dec7
Mark <55669250+MarkCoding337@users.noreply.github.com>
2025-11-18 22:41:13 +0000

    mod

--- gamePage/campusWatchers/script.js
+++ gamePage/campusWatchers/script.js
@@ -1,9 +1,11 @@
 var g = {
     camera: {},
+    computer: {},
 };
 window.onload = function() {
+    //Camera Viewer Setup
     g.camera.ele = document.getElementById('cameraViewer');
-    g.camera.width = g.camera.ele.clientHeight*3; // 300% of container
+    g.camera.width = g.camera.ele.clientWidth; // 300% of container
     g.camera.height = g.camera.ele.clientHeight; // 100% of container
     g.camera.position = 1;
     g.camera.ele.style.left = `-${g.camera.width/3}px`;
@@ -32,4 +34,79 @@ window.onload = function() {
             }
         }
     }
+    //Computer Monitor Setup
+    g.computer.ele = document.getElementById('monitorOutput');
+    g.computer.lines = [
+        ["System Booting...", 1000],
+        "Loading Campus Watcher OS v0.1.0...",
+        "Establishing Secure Connection...",
+        "Connection Established.",
+        "Welcome, Operator.",
+        "Warning: Unauthorized use of this system is prohibited and may be subject to criminal and civil penalties.",
+        ["Further use indicates consent to these terms.", 1000],
+        ["Initializing Camera Feeds...", 1000],
+        ["Camera Feeds Online.", ["button", "View Camera Feeds", "document.getElementById('cameraSection').style.display = 'block'; document.getElementById('computerSection').style.display = 'none';"]],
+    ];
+    g.computer.currentLine = 0;
+    function typeLine() {
+        if(g.computer.currentLine < g.computer.lines.length) {
+            g.computer.ele.innerHTML += "> ";
+            let line = g.computer.lines[g.computer.currentLine];
+            let i = 0;
+            let wait = 500;
+            var button = null;
+            if(line instanceof Array) {
+                if(line[1] instanceof Number) {
+                    wait = line[1];
+                    line = line[0];
+                } else if(line[1] instanceof Array) {
+                    if(line[1][0] == "button") {
+                        button = document.createElement("button");
+                        button.innerText = line[1][1];
+                        button.functionToRun = line[1][2];
+                        button.onclick = function() {
+                            console.log("Button clicked");
+                            eval(this.functionToRun);
+                            this.disabled = true;
+                        };
+                        line = line[0];
+                    }
+                } else {
+                    line = line[0];
+                }
+            };
+            let interval = setInterval(() => {
+                if(i < line.length) {
+                    g.computer.ele.innerHTML += line.charAt(i);
+                    i++;
+                } else {
+                    clearInterval(interval);
+                    g.computer.ele.innerHTML += "<br/>";
+                    g.computer.currentLine++;
+                    let gee;
+                    if(button != null) {
+                        gee = g.computer.ele.appendChild(button);
+                        g.computer.ele.innerHTML += "<br/>";
+                        button = null;
+                        gee.onclick();
+                    }
+                    setTimeout(typeLine, wait);
+                }
+            }, 1);
+        }
+    }
+    typeLine();
+    setInterval(function() {
+        let ele = document.getElementById('monitorMain');
+        if(ele.scrollTop > 0) {
+            document.getElementById('scrollTop').style.display = 'block';
+        } else {
+            document.getElementById('scrollTop').style.display = 'none';
+        }
+        if(ele.scrollTop + ele.clientHeight < ele.scrollHeight) {
+            document.getElementById('scrollBottom').style.display = 'block';
+        } else {
+            document.getElementById('scrollBottom').style.display = 'none';
+        }
+    }, 10)
 }
